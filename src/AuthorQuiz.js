940b6f8bca680b030cca2c7e7cfb416a0409457f
Yongliang <yonglianglee@gmail.com>
2019-03-12 21:59:40 -0500

    add images

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -14,24 +14,72 @@ function Hero(props) {
   );
 }
 
-function Turn() {
-  return <div />;
+function Turn({ author, books, highlight, onAnwserSelected }) {
+  function hightlightBgColor(highlight) {
+    const mapping = {
+      none: "",
+      correct: "green",
+      wrong: "red"
+    };
+    return mapping[highlight];
+  }
+
+  return (
+    <div
+      className="row turn"
+      style={{ backgroundColor: hightlightBgColor(highlight) }}
+    >
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="author" />
+      </div>
+      <div className="col-6">
+        {books.map(title => (
+          <Book title={title} key={title} onClick={onAnwserSelected} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Book({ title, onClick }) {
+  return (
+    <div className="answer" onClick={onClick({ title })}>
+      <h4>{title}</h4>
+    </div>
+  );
 }
 
 function Continue() {
   return <div />;
 }
 
-class AuthorQuiz extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <Hero />
-        <Turn />
-        <Continue />
-      </div>
-    );
-  }
+function Footer() {
+  return (
+    <div id="footer" className="row">
+      <p className="text-muted credit">
+        All images are from
+        <a href="http://commons.wikimedia.org/wiki/Main_Page">
+          Wikemedia Commons
+        </a>
+        and are in the public domain
+      </p>
+    </div>
+  );
+}
+
+function AuthorQuiz({ turnData, highlight, onAnwserSelected }) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn
+        {...turnData}
+        highlight={highlight}
+        onAnwserSelected={onAnwserSelected}
+      />
+      <Continue />
+      <Footer />
+    </div>
+  );
 }
 
 export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,77 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import AuthorQuiz from "./AuthorQuiz";
 import * as serviceWorker from "./serviceWorker";
+import { shuffle, sample } from "underscore";
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById("root"));
+const authors = [
+  {
+    name: "Mark Twain",
+    imageUrl: "images/authors/marktwain.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["The Adventures of Huckleberry Finn"]
+  },
+  {
+    name: "Joseph Conrad",
+    imageUrl: "images/authors/josephconrad.png",
+    imageSource: "Wikimedia Commons",
+    books: ["Heart of Darkness"]
+  },
+  {
+    name: "J.K. Rowling",
+    imageUrl: "images/authors/jkrowling.jpg",
+    imageSource: "Wikimedia Commons",
+    imageAttribution: "Daniel Ogren",
+    books: ["Harry Potter and the Sorcerers Stone"]
+  },
+  {
+    name: "Stephen King",
+    imageUrl: "images/authors/stephenking.jpg",
+    imageSource: "Wikimedia Commons",
+    imageAttribution: "Pinguino",
+    books: ["The Shining", "IT"]
+  },
+  {
+    name: "Charles Dickens",
+    imageUrl: "images/authors/charlesdickens.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["David Copperfield", "A Tale of Two Cities"]
+  },
+  {
+    name: "William Shakespeare",
+    imageUrl: "images/authors/williamshakespeare.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Hamlet", "Macbeth", "Romeo and Juliet"]
+  }
+];
+
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function(p, c, j) {
+    return p.concat(c.books);
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+  return {
+    author: authors.find(author =>
+      author.books.some(title => title === answer)
+    ),
+    books: fourRandomBooks
+  };
+}
+
+function onAnswerSelected(answer) {
+  const isCorrect = this.state.turnData.author.books.some(
+    title => title === answer
+  );
+  this.state.highlight = isCorrect ? "correct" : "wrong";
+}
+
+const state = {
+  turnData: getTurnData(authors),
+  highlight: "correct",
+  onAnswerSelected: onAnswerSelected
+};
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById("root"));
 
 // If you want your AuthorQuiz to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
